8b2370544012a693abab77adf9e58b4fccf82b65
Hamza <82061179+hxxmz@users.noreply.github.com>
2024-02-19 11:02:33 +0500

    up1

---
+++ FileIO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            string line;
+            string path = "C:\\Users\\Denim Admin\\Desktop\\textFile.txt";
+            
+            // Reading
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: ", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing read 'finally' block.");
+            }
+
+            // Writing 
+            try
+            {
+                StreamWriter sw = new StreamWriter(path, true, Encoding.ASCII);
+                sw.WriteLine("");
+                sw.WriteLine("Writing the firstLine on Write Mode.");
+                sw.WriteLine("From the StreamWriter class.");
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: ", ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Executing write 'finally' block.");
+            }
+
+        }
+    }
+}
